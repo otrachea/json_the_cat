37434a1f511644ad00cea33b7147a6d3865f0720
otrachea <francesca.ho459@gmail.com>
2022-03-17 19:07:46 +0000

    Refactor and add tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,23 @@
 const request = require("request");
 
-const requestBreed = (breed) => {
+const fetchBreedDescription = (breed, f) => {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
 
     if (error) {
-      console.log(`Error: ${error}`);
+      f(`Error: ${error}`, null);
       return;
     }
 
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log("Breed not found");
+      f("Breed not found", null);
       return;
     }
 
-    console.log(data[0].description);
+    f(null, data[0].description);
 
   });
 };
 
-requestBreed(process.argv[2]);
+module.exports = { fetchBreedDescription };
